86822fdcdfbbb728950c67c613bc7e6d8019c963
김준형 <wnsgud6232@naver.com>
2023-07-24 17:14:48 +0900

    Feat: 기본 틀 제작

--- frontend/src/components/signUp/SignUp.jsx
+++ frontend/src/components/signUp/SignUp.jsx
@@ -1,9 +1,20 @@
-import react from "react";
+import ProfilePick from './ProfilePick';
+import { Link } from 'react-router-dom';
+
+import classes from './SignUp.module.css'
 
 const SignUp = () => {
   return (
     <div>
-      <h2>회원가입</h2>
+      <h1>회원가입</h1>
+      <p>회원 정보를 확인해주세요</p>
+      <form className={classes.formStyle}>
+        <input type="text" name="email" className={classes.box} />
+        <input type="text" name="name" className={classes.box} />
+        <input type="text" name="phone" className={classes.box} />
+        <input type="text" name="birth" className={classes.box} />
+        <Link to="./ProfilePick.jsx">다음으로</Link>
+      </form>
     </div>
   );
 };
