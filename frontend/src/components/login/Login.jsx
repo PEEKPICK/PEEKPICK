d2d3b9608b07c252733b1722c103939c78c11ab6
김준형 <wnsgud6232@naver.com>
2023-07-24 16:12:19 +0900

    Feat: css -> module.css로 변경

--- frontend/src/components/login/Login.jsx
+++ frontend/src/components/login/Login.jsx
@@ -1,10 +1,11 @@
 import react from "react";
-import './Login.css'
+import classes from "./Login.module.css"
 
 const Login = () => {
   return (
-    <div className="h-screen w-screen bg-[url('./assets/loginBackgroundImage.png')] bg-cover">
-      <div className="container">
+    // <div className="h-screen w-screen bg-[url('./assets/loginBackgroundImage.png')] bg-cover">
+    <div className={classes.background}>
+      <div className={classes.container}>
         <button>카카오 로그인</button>
         <button>네이버 로그인</button>
         <button>구글 로그인</button>
