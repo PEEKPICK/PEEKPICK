6f999f4edbad2af5e9047a30fb54401654e3ccdf
김준형 <wnsgud6232@naver.com>
2023-07-24 15:40:54 +0900

    Feat: login, signup Router 설정

--- frontend/src/components/login/Login.jsx
+++ frontend/src/components/login/Login.jsx
@@ -1,7 +1,16 @@
 import react from "react";
+import './Login.css'
 
 const Login = () => {
-
+  return (
+    <div className="h-screen w-screen bg-[url('./assets/loginBackgroundImage.png')] bg-cover">
+      <div className="container">
+        <button>카카오 로그인</button>
+        <button>네이버 로그인</button>
+        <button>구글 로그인</button>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
--- frontend/src/components/signUp/SignUp.jsx
+++ frontend/src/components/signUp/SignUp.jsx
@@ -2,7 +2,9 @@ import react from "react";
 
 const SignUp = () => {
   return (
-    
+    <div>
+      <h2>회원가입</h2>
+    </div>
   );
 };
 
