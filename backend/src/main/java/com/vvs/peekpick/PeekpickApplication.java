4780c2612d4c530aa832f1ccd8349aef399843f1
97kzone <mmnm3@naver.com>
2023-07-24 10:25:04 +0900

    Feat: 프로젝트 시작

---
+++ backend/src/main/java/com/vvs/peekpick/PeekpickApplication.java
@@ -0,0 +1,13 @@
+package com.vvs.peekpick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeekpickApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeekpickApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/vvs/peekpick/PeekpickApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vvs.peekpick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeekpickApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
