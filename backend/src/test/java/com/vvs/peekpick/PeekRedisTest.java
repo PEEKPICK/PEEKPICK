2a289e016c1669cad4cdd027c4115a3c803ce3d8
Dayoung <everyyoung99@gmail.com>
2023-07-25 23:20:18 +0900

    Test: Peek 등록, 조회, 삭제 테스트 구현

--- backend/src/test/java/com/vvs/peekpick/PeekRedisTest.java
+++ backend/src/test/java/com/vvs/peekpick/PeekRedisTest.java
@@ -42,5 +42,31 @@ public class PeekRedisTest {
         locationTemplate.delete("PeekLocation");
     }
 
+    @Test
+    public void testAddGetDeletePeek() {
+        PeekLocationDto peekLocationDto = new PeekLocationDto(1L, new Point(127.0, 37.0));
+        PeekDto peekDto = new PeekDto(1L, 1L, "content", "imageUrl", 0, 0, LocalDateTime.now());
+        peekRedisService.addPeek(peekLocationDto, peekDto);
+
+        Point memberLocation = geoOps.geoPos("PeekLocation", peekLocationDto.getPeekId().toString()).get(0);
+        assertEquals(peekLocationDto.getPoint().getX(), memberLocation.getX(), 0.0001);
+        assertEquals(peekLocationDto.getPoint().getY(), memberLocation.getY(), 0.0001);
+
+        PeekDto getPeekDto = peekRedisService.findPeekById(peekDto.getPeekId());
+        assertEquals(peekDto.getPeekId(), getPeekDto.getPeekId());
+        assertEquals(peekDto.getMemberId(), getPeekDto.getMemberId());
+        assertEquals(peekDto.getContent(), getPeekDto.getContent());
+        assertEquals(peekDto.getImageUrl(), getPeekDto.getImageUrl());
+        assertEquals(peekDto.getLikeCount(), getPeekDto.getLikeCount());
+        assertEquals(peekDto.getDisLikeCount(), getPeekDto.getDisLikeCount());
+        assertEquals(peekDto.getWriteTime().withNano(0), getPeekDto.getWriteTime().withNano(0));
+
+        peekRedisService.deletePeek(peekDto.getPeekId());
+        assertNull(peekRedisService.findPeekById(peekDto.getPeekId()));
+        List<Point> result = geoOps.geoPos("PeekLocation", peekLocationDto.getPeekId().toString());
+        assertTrue(result == null || result.isEmpty());
+    }
+
+
 
 }
