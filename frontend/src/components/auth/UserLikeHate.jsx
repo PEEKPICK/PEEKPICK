2b42a0ea5fccc0a357f2a5f8a35fc4d5d6605a04
김준형 <wnsgud6232@naver.com>
2023-07-25 15:51:14 +0900

    Feat: UserLikeHate 생성 / UserNickname 기본 틀, css 작성 / App.js 라우터 추가

---
+++ frontend/src/components/auth/UserLikeHate.jsx
@@ -0,0 +1,9 @@
+const UserLikeHate = () => {
+  return (
+    <div>
+      <h1>UserLikeHate</h1>
+    </div>
+  );
+};
+
+export default UserLikeHate;
--- frontend/src/components/auth/UserNickname.jsx
+++ frontend/src/components/auth/UserNickname.jsx
@@ -1,7 +1,37 @@
+import { useNavigate } from 'react-router-dom';
+
+import classes from './UserNickname.module.css';
+
 const UserNickname = () => {
+  const navigate = useNavigate();
+
+  const moveToUserLikeHate = () => {
+    navigate('/userlikehate')
+  }
+
   return (
     <div>
-      <h1>UserNickname</h1>
+      <div>
+        <h1>닉네임 PICK</h1>
+      </div>
+      <div>
+        <p>자신을 나타낼 수 있는 정보는 최대한 삼가해주세요!</p>
+      </div>
+      <div className={classes.line}></div>
+      <div>
+        <h3>타이들</h3>
+        <div>
+          <input type="text" />
+          <img src="img/reloadWhite.png" alt="reload" className={classes.reload} />
+        </div>
+      </div>
+      <div>
+        <h3>닉네임</h3>
+        <input type="text" />
+      </div>
+      <div>
+        <button onClick={moveToUserLikeHate}>다음으로</button>
+      </div>
     </div>
   );
 }
