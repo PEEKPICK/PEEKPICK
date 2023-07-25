28e8370acf95fcd4784263e21b0b2b774ca5ba77
김준형 <wnsgud6232@naver.com>
2023-07-25 15:16:59 +0900

    Feat: UserInfo 기본 틀 작성 완료 / UserInfo css 기본 작성

--- frontend/src/components/auth/UserInfo.jsx
+++ frontend/src/components/auth/UserInfo.jsx
@@ -1,8 +1,32 @@
+import { useNavigate } from 'react-router-dom';
+
+import classes from './UserInfo.module.css';
+
 const UserInfo = () => {
+  const navigate = useNavigate();
+
+  const moveToUserProfile = () => {
+    navigate('/userprofile')
+  };
+
   return (
     <div>
-      <h1>회원가입</h1>
-      <p>회원 정보를 확인해주세요</p>
+      <div>
+        <h1>회원가입</h1>
+      </div>
+      <div>
+        <p>회원 정보를 확인해주세요</p>
+      </div>
+      <div>
+        <form className={classes.form}>
+          <input type="text" name="email" value="이메일" />
+          <input type="text" name="name" value="이름" />
+          <input type="text" name="phone" value="전화번호" />
+          <input type="text" name="birth" value="생년월일" />
+          <input type="text" name="sex" value="성별"/>
+          <input type="button" value="다음으로" onClick={moveToUserProfile} />
+        </form>
+      </div>
     </div>
   );
 }
