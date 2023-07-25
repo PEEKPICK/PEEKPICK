c68ff4159e9dc023a92579b39c9553465b5ae52e
김준형 <wnsgud6232@naver.com>
2023-07-25 15:34:08 +0900

    Feat: UserProfile 기본 틀 작성, css 작성 / 랜덤으로 이모지 뽑기 구현

--- frontend/src/components/auth/UserProfile.jsx
+++ frontend/src/components/auth/UserProfile.jsx
@@ -1,7 +1,39 @@
+import { useState } from 'react';
+
+import classes from './UserProfile.module.css';
+
 const UserProfile = () => {
+  const [randomEmoji, setRandomEmoji] = useState('https://peekpick-app.s3.ap-northeast-2.amazonaws.com/Astonished+Face.png');
+
+  // 이모지 다시 뽑기를 위한 버튼
+  const randomGacha = () => {
+    var emoji = ['https://peekpick-app.s3.ap-northeast-2.amazonaws.com/Astonished+Face.png',
+                'https://peekpick-app.s3.ap-northeast-2.amazonaws.com/Biting+Lip.png',
+                'https://peekpick-app.s3.ap-northeast-2.amazonaws.com/Cat+with+Tears+of+Joy.png']
+    var random_index = Math.floor(Math.random() * emoji.length);
+    var selectedEmoji = emoji[random_index]
+    setRandomEmoji(selectedEmoji);
+  };
+
   return (
     <div>
-      <h1>UserProfile</h1>
+      <div>
+        <h1>프로필 PICK</h1>
+      </div>
+      <div>
+        <p>개성넘치는 프로필을 뽑아보세요.</p>
+        <p>숨겨진 프로필이 있다는 소문이...</p>
+      </div>
+      <div className={classes.line}></div>
+      <div>
+        <img src={randomEmoji} alt="dummy_emoji" />
+      </div>
+      <div>
+        <button onClick={randomGacha}>다시뽑기</button>
+      </div>
+      <div>
+        <button>다음으로</button>
+      </div>
     </div>
   );
 }
