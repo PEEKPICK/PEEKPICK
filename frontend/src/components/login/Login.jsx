5c69fa113c0e6cdf051f8d6a7cbe8406830faa9e
김준형 <wnsgud6232@naver.com>
2023-07-25 09:05:15 +0900

    Feat: Login.jsx 주석 추가 - css변경 예정

--- frontend/src/components/login/Login.jsx
+++ frontend/src/components/login/Login.jsx
@@ -1,10 +1,9 @@
-import react from "react";
 import classes from "./Login.module.css"
 
 const Login = () => {
   return (
-    // <div className="h-screen w-screen bg-[url('./assets/loginBackgroundImage.png')] bg-cover">
-    <div className={classes.background}>
+    <div className="h-screen w-screen bg-[url('./assets/loginBackgroundImage.png')] bg-cover">
+      {/* 백그라운드 이미지 CSS 변경 예정 - tailwindcss -> css */}
       <div className={classes.container}>
         <button>카카오 로그인</button>
         <button>네이버 로그인</button>
