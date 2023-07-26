8110bc05ff1835a637151205d604b0e2654271a0
김준형 <wnsgud6232@naver.com>
2023-07-26 17:21:24 +0900

    Feat: axios 설치 / Login.jsx 더미 API 호출 코드 작성

--- frontend/src/components/auth/Login.jsx
+++ frontend/src/components/auth/Login.jsx
@@ -1,8 +1,16 @@
+import axios from 'axios';
+
 import classes from './style/Login.module.css';
 
 const Login = () => {
   const kakao = () => {
-    console.log('kakao')
+    axios.get('https://jsonplaceholder.typicode.com/todos/1')
+      .then(response => {
+        console.log(response.data)
+      })
+      .catch(error => {
+        console.log(error)
+      })
   };
 
   const naver = () => {
