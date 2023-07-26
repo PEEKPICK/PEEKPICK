e255aeef2e991f9635dab769de87a6f79e98154c
김준형 <wnsgud6232@naver.com>
2023-07-26 09:38:40 +0900

    Feat: UserLike, UserHate 기본 틀 완성, 공통 css 기본 틀 작성

--- frontend/src/components/auth/UserLike.jsx
+++ frontend/src/components/auth/UserLike.jsx
@@ -1,7 +1,24 @@
+import { useNavigate } from 'react-router-dom';
+
+import classes from './style/UserLikeOrHate.module.css';
+
 const UserLike = () => {
+  const navigate = useNavigate();
+
+  const selectedFinish = () => {
+    navigate('/userlikehate');
+  };
+
   return (
     <div>
-      <h1>UserLike</h1>
+      <div>
+        <h1>좋아해요</h1>
+        <p>(최대 5개 선택 가능)</p>
+      </div>
+      <div className={classes.line}></div>
+      <div>
+        <button onClick={selectedFinish}>선택완료</button>
+      </div>
     </div>
   );
 }
