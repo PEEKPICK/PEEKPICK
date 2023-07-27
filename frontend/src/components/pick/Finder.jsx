6afe4f7541a76b77856cde8c0f9333659e15a923
김동민 <anwjr41@naver.com>
2023-07-28 01:44:21 +0900

    Feat: picker 랜던뽑기

--- frontend/src/components/pick/Finder.jsx
+++ frontend/src/components/pick/Finder.jsx
@@ -1,7 +1,7 @@
-import classes from './Finder.module.css';
-import React, { useEffect, useState } from 'react';
+import classes from "./Finder.module.css";
+import React, { useEffect, useState } from "react";
 
-import RandomEmoji from './RandomEmoji';
+import RandomEmoji from "./RandomEmoji";
 
 function Finder() {
   const [emoji, setEmoji] = useState(null);
@@ -13,13 +13,8 @@ function Finder() {
 
   const fetchEmoji = () => {
     setLoading(true);
-    fetch('http://localhost:8080/posts') // API 엔드포인트 URL
-      .then(
-        (response) =>
-          new Promise((resolve) =>
-            setTimeout(() => resolve(response.json()), 500)
-          )
-      )
+    fetch("http://localhost:8080/posts") // API 엔드포인트 URL
+      .then((response) => new Promise((resolve) => setTimeout(() => resolve(response.json()), 500)))
       //정상 응답
       .then((jsonEmoji) => {
         setEmoji(jsonEmoji.posts);
@@ -27,19 +22,15 @@ function Finder() {
       })
       //에러
       .catch((error) => {
-        console.error('Error fetching Emoji:', error);
+        console.error("Error fetching Emoji:", error);
         setLoading(false);
       });
-  };
-
-  const refreshHandler = () => {
-    fetchEmoji();
   };
 
   return (
     <>
       <div className={classes.finderMain}>
-        <button className={classes.button} onClick={refreshHandler}>
+        <button className={classes.button} onClick={fetchEmoji}>
           <img src="/img/reloadBlue.png" alt="새로고침" />
           피커찾기
         </button>
--- frontend/src/components/pick/RandomEmoji.jsx
+++ frontend/src/components/pick/RandomEmoji.jsx
@@ -6,22 +6,19 @@ function RandomEmoji({ EmojiData, Loading }) {
   }
 
   if (EmojiData === null) {
-    return <div>No emoji data available.</div>;
+    return <div>ÅÖ...!!!</div>;
   }
 
-  const shuffledData = [...EmojiData].sort(() => 1.0 - Math.random());
-  const limitedData = shuffledData.slice(0, 10);
-
-  const gridSize = 10; // °¡·Î¿Í ¼¼·Î¿¡ 10Ä­¾¿ ÃÑ 100Ä­
+  const gridSize = 7; // °¡·Î¿Í ¼¼·Î¿¡ 10Ä­¾¿ ÃÑ 100Ä­
   const cellSize = `${100 / gridSize}%`;
 
   const getRandomIndexes = (max, count) => {
     const indexes = Array.from({ length: max }, (_, i) => i);
-    const shuffledIndexes = indexes.sort(() => 0.5 - Math.random());
+    const shuffledIndexes = shuffleArray(indexes);
     return shuffledIndexes.slice(0, count);
   };
 
-  const randomIndexes = getRandomIndexes(gridSize * gridSize, 5);
+  const randomIndexes = getRandomIndexes(gridSize * gridSize, 10);
 
   return (
     <div
@@ -32,11 +29,18 @@ function RandomEmoji({ EmojiData, Loading }) {
       {Array.from({ length: gridSize * gridSize }).map((_, index) => (
         <div key={index}>
           {/* ·£´ýÇÑ 5°³ÀÇ ¼¿¿¡¸¸ limitedData.id ¹èÄ¡ */}
-          {randomIndexes.includes(index) ? limitedData[Math.floor(Math.random() * limitedData.length)].id : null}
+          {randomIndexes.includes(index) ? EmojiData[Math.floor(Math.random() * EmojiData.length)].id : null}
         </div>
       ))}
     </div>
   );
 }
-
+// Fisher-Yates (Knuth) Shufflem
+const shuffleArray = (array) => {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+};
 export default RandomEmoji;
