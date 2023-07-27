727720a8f72bba161448a22fe958355d5b59ed2d
KyongBeom <dragontig98@naver.com>
2023-07-27 17:25:32 +0900

    Feat: signout 일부 구현

--- frontend/src/components/mypages/SignOut.jsx
+++ frontend/src/components/mypages/SignOut.jsx
@@ -1,6 +1,12 @@
-import React,{useEffect,useRef,forwardRef} from 'react';
+import React,{useEffect,useRef,forwardRef,useState} from 'react';
 import classes from './SignOut.module.css';
+import { Link } from 'react-router-dom';
 const SignOut = forwardRef((props,ref) => {
+
+  const [isChecked, setIsChecked] = useState(false);
+  const handleCheckboxChange = () => {
+    setIsChecked(!isChecked);
+  }
   let wrapperRef = useRef();
     useEffect(()=>{
       document.addEventListener('mousedown', handleClickOutside);
@@ -29,15 +35,23 @@ const SignOut = forwardRef((props,ref) => {
         <p>
           회원 탈퇴 어쩌구 저쩌구 모든 정보가 삭제 어쩌구
         </p>
-        <input type="checkbox" />
+        <input type="checkbox" checked={isChecked} onChange={handleCheckboxChange}/>
         {/* 체크박스 활성화시 회원탈퇴 버튼 색 활성화 */}
         <p>위 유의사항을 모두 확인하였고, 탈퇴를 진행합니다.</p>
       </div>
       {/* 색깔 변화 넣어야함 */}
-      <div>
+      {isChecked ?(
+        <Link to="/login">
+        <div>
         {/* 클릭 시 DB 계정 정보 삭제 */}
         <h4>회원탈퇴</h4>
       </div>
+      </Link>
+      ):(
+        <div>
+          <h4>회원탈퇴</h4>
+        </div>
+      )}
     </div>
   );
 })
