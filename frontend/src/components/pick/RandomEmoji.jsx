ea7910fc9534500c733770963a1b28da805ccbf1
kim <anwjr41@naver.com>
2023-07-28 01:03:40 +0900

    Feat: picker 랜덤 뽑기

--- frontend/src/components/pick/RandomEmoji.jsx
+++ frontend/src/components/pick/RandomEmoji.jsx
@@ -1,26 +1,42 @@
-import classes from './EmojiArea.module.css';
+import classes from "./RandomEmoji.module.css";
 
-function RandomEmoji(props) {
-  // ìµœëŒ€ 5ê°œê¹Œì§€ ëœë¤ìœ¼ë¡œ ë³´ì—¬ì£¼ê¸° ìœ„í•´ dataë¥¼ ëœë¤í•˜ê²Œ ì„ìŒ
-  const { EmojiData } = props;
-
-  if (EmojiData !== null) {
-    // ëœë¤í•˜ê²Œ emoji ë°ì´í„° ì„ íƒ
-    const shuffledData = [...EmojiData].sort(() => 0.5 - Math.random());
-    const limitedData = shuffledData.slice(0, 5);
-
-    return (
-      // dataì˜ ê¸¸ì´ê°€ 1ë³´ë‹¤ í° ê²½ìš° ìµœëŒ€ 5ê°œì˜ ê°’ì„ ë‚˜ì—´
-      <div className={classes.emojiArea}>
-        {limitedData.map((limitedData) => (
-          <div key={limitedData.id}>
-            <span>{limitedData.author}</span>
-            <span>{limitedData.body}</span>
-          </div>
-        ))}
-      </div>
-    );
+function RandomEmoji({ EmojiData, Loading }) {
+  if (Loading) {
+    return <div>Loading...</div>;
   }
+
+  if (EmojiData === null) {
+    return <div>No emoji data available.</div>;
+  }
+
+  const shuffledData = [...EmojiData].sort(() => 1.0 - Math.random());
+  const limitedData = shuffledData.slice(0, 10);
+
+  const gridSize = 10; // °¡·Î¿Í ¼¼·Î¿¡ 10Ä­¾¿ ÃÑ 100Ä­
+  const cellSize = `${100 / gridSize}%`;
+
+  const getRandomIndexes = (max, count) => {
+    const indexes = Array.from({ length: max }, (_, i) => i);
+    const shuffledIndexes = indexes.sort(() => 0.5 - Math.random());
+    return shuffledIndexes.slice(0, count);
+  };
+
+  const randomIndexes = getRandomIndexes(gridSize * gridSize, 5);
+
+  return (
+    <div
+      className={classes.emojiArea}
+      style={{ display: "grid", gridTemplateColumns: `repeat(${gridSize}, ${cellSize})` }}
+    >
+      {/* 100Ä­À¸·Î ºĞÇÒµÈ div ¾È¿¡ ·£´ıÇÑ 5°³ µ¥ÀÌÅÍ ¹èÄ¡ */}
+      {Array.from({ length: gridSize * gridSize }).map((_, index) => (
+        <div key={index}>
+          {/* ·£´ıÇÑ 5°³ÀÇ ¼¿¿¡¸¸ limitedData.id ¹èÄ¡ */}
+          {randomIndexes.includes(index) ? limitedData[Math.floor(Math.random() * limitedData.length)].id : null}
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default RandomEmoji;
