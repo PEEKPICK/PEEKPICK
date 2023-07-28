97b311bd914014b2a1920e51f40d410af1705ce7
KyongBeom <dragontig98@naver.com>
2023-07-28 17:02:00 +0900

    Feat: NavBar 클릭시 반응

--- frontend/src/components/NavigationBar.jsx
+++ frontend/src/components/NavigationBar.jsx
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 // import ChartAlert from './ChatAlert';
 import { Link } from 'react-router-dom';
 import classes from './NavigationBar.module.css';
 const NavigationBar = () => {
+  const [picky, setPicky] = useState(false);
+  const [picker, setPicker] = useState(true);
+  const [mypage, setMypage] = useState(false);
 
+  const pickyClick = () => {
+    setPicky(true);
+    setPicker(false);
+    setMypage(false);
+  }
+  const pickerClick = () => {
+    setPicky(false);
+    setPicker(true);
+    setMypage(false);
+  }
+  const mypageClick = () => {
+    setPicky(false);
+    setPicker(false);
+    setMypage(true);
+  }
   return (
     <div className={classes.basic}>
       <div>
@@ -13,28 +31,48 @@ const NavigationBar = () => {
 
       {/* 개별적으로 클릭시 그쪽 색상 들어오게 div css 설정 잘하기 */}
       <nav className={classes.navi}>
-      <Link to="picky" className={classes.self}>
-        <div>
-          <img src="img/heartBlack.png" alt="" />
-          {/* <img src="img/heartWhite.png" alt="" /> */}
-          <p>이슈</p>
-        </div>
-      </Link>
-      <Link to={""} className={classes.self}>
-        <div>
-          <img src="img/finderBlack.png" alt="" />
-          {/* <img src="img/finderWhite.png" alt="" /> */}
-          <img src="" alt="" />
-          <p>사람</p>
-        </div>
-      </Link>
-      <Link to={"mypage"} className={classes.self}>
-        <div>
+        {picky ? <Link to="picky" className={classes.self}>
+          <div onClick={pickyClick}>
+            {/* <img src="img/heartBlack.png" alt="" /> */}
+            <img src="img/heartWhite.png" alt="" />
+            <p>이슈</p>
+          </div>
+        </Link> :
+          <Link to="picky" className={classes.self}>
+            <div onClick={pickyClick}>
+              <img src="img/heartBlack.png" alt="" />
+              {/* <img src="img/heartWhite.png" alt="" /> */}
+              <p>이슈</p>
+            </div>
+          </Link>}
+        {picker ? <Link to={""} className={classes.self}>
+          <div onClick={pickerClick}>
+            {/* <img src="img/finderBlack.png" alt="" /> */}
+            <img src="img/finderWhite.png" alt="" />
+            <p>사람</p>
+          </div>
+        </Link> :
+          <Link to={""} className={classes.self}>
+            <div onClick={pickerClick}>
+              <img src="img/finderBlack.png" alt="" />
+              {/* <img src="img/finderWhite.png" alt="" /> */}
+              <p>사람</p>
+            </div>
+          </Link>}
+        {mypage ? <Link to={"mypage"} className={classes.self}>
+          <div onClick={mypageClick}>
+            {/* <img src="img/mypageBlack.png" alt="" /> */}
+            <img src="img/mypageWhite.png" alt="" />
+            <p>프로필</p>
+          </div>
+        </Link> :
+        <Link to={"mypage"} className={classes.self}>
+        <div onClick={mypageClick}>
           <img src="img/mypageBlack.png" alt="" />
           {/* <img src="img/mypageWhite.png" alt="" /> */}
           <p>프로필</p>
         </div>
-      </Link>
+      </Link>}
       </nav>
     </div>
   );
