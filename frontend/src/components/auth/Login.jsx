ceee973d8f63263583b35d1b4f5383df2d86f3ad
김준형 <wnsgud6232@naver.com>
2023-08-01 12:50:21 +0900

    Test: 로그인 테스트 버튼 생성

--- frontend/src/components/auth/Login.jsx
+++ frontend/src/components/auth/Login.jsx
@@ -1,3 +1,5 @@
+import { customAxios } from '../../api/customAxios';
+
 import classes from './style/Login.module.css';
 
 const Login = () => {
@@ -5,10 +7,27 @@ const Login = () => {
       window.location.href = `http://localhost:8081/oauth2/authorization/${service}`;
   };
 
+  // 테스트 버튼
+  const test = (id) => {
+    const dummy = {
+      id: 1,
+      content: '더미입니다.'
+    }
+
+    customAxios.post(`/member/login?id=test${id}`, dummy)
+      .then(response => {
+        console.log(response)
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }
+
   return (
     <div className={classes.container}>
       <div className={classes.buttons}>
         {/* 이미지로 변경 예정 */}
+        <button onClick={() => test(1)}>테스트 버튼</button>
         <button onClick={() => loginHandler('kakao')}>카카오 로그인</button>
         <button onClick={() => loginHandler('naver')}>네이버 로그인</button>
         <button onClick={() => loginHandler('google')}>구글 로그인</button>
