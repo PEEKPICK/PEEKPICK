4c1cf54e377f709f34d0ced54bb2dbfa01f173a0
김준형 <wnsgud6232@naver.com>
2023-08-01 13:06:51 +0900

    Test: 테스트 input 생성

--- frontend/src/components/auth/Login.jsx
+++ frontend/src/components/auth/Login.jsx
@@ -1,8 +1,12 @@
+import { useState } from 'react';
 import { customAxios } from '../../api/customAxios';
 
 import classes from './style/Login.module.css';
 
 const Login = () => {
+  // 테스트용 상태 관리
+  const [tester, setTester] = useState(1);
+
   const loginHandler = (service) => {
       window.location.href = `http://localhost:8081/oauth2/authorization/${service}`;
   };
@@ -27,7 +31,8 @@ const Login = () => {
     <div className={classes.container}>
       <div className={classes.buttons}>
         {/* 이미지로 변경 예정 */}
-        <button onClick={() => test(1)}>테스트 버튼</button>
+        <input type="text" onChange={e => setTester(e.target.value)} />
+        <button onClick={() => test(tester)}>테스트 버튼</button>
         <button onClick={() => loginHandler('kakao')}>카카오 로그인</button>
         <button onClick={() => loginHandler('naver')}>네이버 로그인</button>
         <button onClick={() => loginHandler('google')}>구글 로그인</button>
