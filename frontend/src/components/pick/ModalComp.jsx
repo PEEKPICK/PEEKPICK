13264885b50175d9d1be1c2d34e7cc94d7b477ea
김동민 <anwjr41@naver.com>
2023-08-03 02:08:29 +0900

    Feat: 프로필 모달 CSS

--- frontend/src/components/pick/ModalComp.jsx
+++ frontend/src/components/pick/ModalComp.jsx
@@ -9,6 +9,7 @@ const ModalComp = () => {
   const dispatch = useDispatch();
 
   console.log("선택이모지", isSelectedEmoji);
+
   const handleCloseModal = () => {
     dispatch(modalActions.closeModal());
   };
@@ -18,33 +19,47 @@ const ModalComp = () => {
       {isModalState && isSelectedEmoji && (
         <Modal
           isOpen={isModalState}
-          onRequestClose={handleCloseModal} // 모달 바깥을 클릭하거나 ESC 키를 누르면 모달을 닫음
+          onRequestClose={() => handleCloseModal()} // 모달 바깥을 클릭하거나 ESC 키를 누르면 모달을 닫음
           contentLabel="Selected Emoji Modal"
           className={classes.modalMain}
         >
-          <div>
-            <img src="./img/cancel.png" alt="??" onClick={handleCloseModal} className={classes.closeBtn} />
-          </div>
           {/* 모달 내용에 선택된 avatarId를 표시 */}
-          <img src={isSelectedEmoji.emoji.imageUrl} alt="프로필" className={classes.profileImg} />
-          <p>avatarId: {isSelectedEmoji.avatarId}</p>
-          <p>
-            nickname: {isSelectedEmoji.prefix.content} {isSelectedEmoji.nickname}
-          </p>
-          <p>
-            Likes:{" "}
-            {isSelectedEmoji.likes.map((like, index) => (
-              <span key={index}>{like} </span>
-            ))}
-          </p>
-          <p>
-            disLikes:{" "}
-            {isSelectedEmoji.disLikes.map((disLikes, index) => (
-              <span key={index}>{disLikes} </span>
-            ))}
-          </p>
-          <p>Selected: {isSelectedEmoji.avatarId}</p>
-          <p>Selected: {isSelectedEmoji.avatarId}</p>
+          <div className={classes.modalHead}>
+            <img src={isSelectedEmoji.emoji.animatedImageUrl} alt="프로필" className={classes.profileImg} />
+            <div className={classes.modalHeadText}>
+              <span className={classes.nickname}>
+                {isSelectedEmoji.prefix.content} {isSelectedEmoji.nickname}
+              </span>
+              <span style={{ marginRight: "0.2rem" }}>PICK</span>
+              <span style={{ color: "#7d00ff", fontWeight: "700" }}>100</span>
+              <span style={{ marginLeft: "0.2rem" }}>회</span>
+              <p className={classes.intro}>지각률 66.6% 보장</p>
+            </div>
+          </div>
+          <div className={classes.divider}></div>
+          <div className={classes.modalBody}>
+            <div>
+              <p className={classes.like}>좋아!</p>
+              <p>
+                {isSelectedEmoji.likes.map((like, index) => (
+                  <span key={index} className={classes.likeInfo}>
+                    #{like}
+                  </span>
+                ))}
+              </p>
+            </div>
+            <div>
+              <p className={classes.disLike}>싫어!</p>
+              <p>
+                {isSelectedEmoji.disLikes.map((disLikes, index) => (
+                  <span key={index} className={classes.likeInfo}>
+                    #{disLikes}{" "}
+                  </span>
+                ))}
+              </p>
+            </div>
+          </div>
+          <div className={classes.pick}>PICK</div>
         </Modal>
       )}
     </>
