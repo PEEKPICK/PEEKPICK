1890eef8cc90ef4b36570854f1a48cdd73911c99
김준형 <wnsgud6232@naver.com>
2023-08-03 17:37:56 +0900

    Style: Modal CSS 작성 완료

--- frontend/src/components/auth/Modal.jsx
+++ frontend/src/components/auth/Modal.jsx
@@ -4,9 +4,10 @@ import classes from './style/Modal.module.css';
 const Modal = ({ onClose }) => {
   return (
     <div className={classes.container}>
-      <img src="img/cancel.png" alt="cancel" onClick={onClose} />
-      <div>
-        <span>최대 5개 선택이 가능합니다.</span>
+      <img src="img/cancel.png" alt="cancel" onClick={onClose} className={classes.cancelImg} />
+      <div className={classes.infoBox}>
+        <img src="img/infoRed.png" alt="info" className={classes.infoImg} />
+        <span className={classes.message}>최대 5개 선택이 가능합니다.</span>
       </div>
     </div>
   );
