5721769f13ed6bc3503e19828346e25f9978d45b
김준형 <wnsgud6232@naver.com>
2023-08-03 15:44:07 +0900

    Test: 테스트 회원가입 버튼 구현

--- frontend/src/components/auth/Login.jsx
+++ frontend/src/components/auth/Login.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react';
 import { customAxios } from '../../api/customAxios';
+import { useNavigate } from 'react-router-dom';
 
 import classes from './style/Login.module.css';
 
 const Login = () => {
   // 테스트용 상태 관리
   const [tester, setTester] = useState(1);
+  const navigate = useNavigate();
 
   const loginHandler = (service) => {
       window.location.href = `http://localhost:8081/oauth2/authorization/${service}`;
@@ -31,12 +33,19 @@ const Login = () => {
       })
   }
 
+  const testSignUp = () => {
+    navigate("/userinfo")
+  }
+
   return (
     <div className={classes.container}>
       <div className={classes.buttons}>
         {/* 이미지로 변경 예정 */}
+        {/* 테스트 버튼 */}
         <input type="text" onChange={e => setTester(e.target.value)} />
         <button onClick={() => test(tester)}>테스트 버튼</button>
+        <button onClick={() => testSignUp()}>테스트 회원가입</button>
+        {/* 테스트 버튼 */}
         <button onClick={() => loginHandler('kakao')}>카카오 로그인</button>
         <button onClick={() => loginHandler('naver')}>네이버 로그인</button>
         <button onClick={() => loginHandler('google')}>구글 로그인</button>
