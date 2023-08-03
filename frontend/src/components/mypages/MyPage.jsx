3376b64c85c2c09f5a0fe04caea7ce953250438d
KyongBeom <dragontig98@naver.com>
2023-08-03 15:32:24 +0900

    Feat: Token API 유효성 검사

--- frontend/src/components/mypages/MyPage.jsx
+++ frontend/src/components/mypages/MyPage.jsx
@@ -7,7 +7,7 @@ import SignOut from './SignOut';
 import PickAndLike from './PickAndLike';
 import NickNameEdit from './NickNameEdit';
 import { customAxios } from '../../api/customAxios';
-import { useDispatch,useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { authActions } from '../../store/authSlice';
 const MyPage = () => {
   // 상태관리
@@ -16,7 +16,7 @@ const MyPage = () => {
   const [signoutView, setSignoutView] = useState(false);
   const [nicknameView, setNicknameView] = useState(false);
   const [ModalOutSide, setModalOutSide] = useState(false);
-  const [useremoji,setUseremoji] = useState("");
+  const [useremoji, setUseremoji] = useState("");
   const userInfo = useSelector(state => state.auth);
   // 정보 확인
   const dispatch = useDispatch();
@@ -25,10 +25,11 @@ const MyPage = () => {
   // 토큰 처리
   const jwtToken = localStorage.getItem('jwtToken');
 
+
   // 이름과 한줄평 가져오는 usestate
-  const [bio,setBio] = useState(userInfo.bio);
-  const [nickname,setNickname] = useState(userInfo.nickname);
-  const [prefix,setPrefix] = useState(userInfo.prefix);
+  const [bio, setBio] = useState(userInfo.bio);
+  const [nickname, setNickname] = useState(userInfo.nickname);
+  const [prefix, setPrefix] = useState(userInfo.prefix);
 
   // 페이지 렌더링 시 작동
   useEffect(() => {
@@ -56,41 +57,52 @@ const MyPage = () => {
 
   // api통신
   useEffect(() => {
+
+    function isTokenExpired() {
+      if (!jwtToken)return true; 
+      const decodedToken = JSON.parse(atob(jwtToken.split('.')[1]));
+      const decoded = decodedToken.exp;
+      const currentTime = Date.now() / 1000;
+      return decoded < currentTime;
+    }
+    
     const headers = {
-      Authorization : `Bearer ${jwtToken}`
+      Authorization: `Bearer ${jwtToken}`
     };
-  
     const fetchData = async () => {
-      try {
-        const response = await customAxios.get("/member/info", { headers });
-        setEmojiUrl(response.data.data.emoji.imageUrl);
-        setUseremoji(response.data.data.emoji.imageUrl);
-        setBio(response.data.data.bio);
-        setNickname(response.data.data.nickname);
-        setPrefix(response.data.data.prefix.content);
-        const sendToMyPageData = {
-          emojiUrl: response.data.data.emoji.imageUrl,
-        };
-        const sendToUserNicknameData={
-          bio:bio,
-          nickname:nickname,
-          prefix:prefix,
+      if (isTokenExpired()) {
+        try {
+          const response = await customAxios.get("/member/info", { headers });
+          setEmojiUrl(response.data.data.emoji.imageUrl);
+          setUseremoji(response.data.data.emoji.imageUrl);
+          setBio(response.data.data.bio);
+          setNickname(response.data.data.nickname);
+          setPrefix(response.data.data.prefix.content);
+          const sendToMyPageData = {
+            emojiUrl: response.data.data.emoji.imageUrl,
+          };
+          const sendToUserNicknameData = {
+            bio: bio,
+            nickname: nickname,
+            prefix: prefix,
+          }
+          dispatch(authActions.updateMyPageProfile(sendToMyPageData));
+          dispatch(authActions.updateUserNickname(sendToUserNicknameData));
+        } catch (error) {
         }
-        dispatch(authActions.updateMyPageProfile(sendToMyPageData));
-        dispatch(authActions.updateUserNickname(sendToUserNicknameData));
-      } catch (error) {
-        console.log(error);
-        console.log(useremoji);
-        console.log(emojiUrl);
+      } else {
+        const response = await customAxios.post("/auth/refresh", { headers });
+        const data = response.data.data;
+        console.log('hi', data);
       }
     };
     fetchData(); // useEffect 내부에서 async 함수 호출
-  }, [dispatch, emojiUrl, useremoji, jwtToken,bio,nickname,prefix]);
+  }, [dispatch, emojiUrl, useremoji, jwtToken, bio, nickname, prefix]);
   return (
     <div className={classes.mypage}>
       {logoutView && <LogOut setLogoutView={setLogoutView} />}
-      {signoutView && <SignOut setSignoutView={setSignoutView}/>}
-      {nicknameView && <NickNameEdit setNicknameView={setNicknameView} propPrefix={setPrefix} propBio={setBio} propNickname={setNickname}/>}
+      {signoutView && <SignOut setSignoutView={setSignoutView} />}
+      {nicknameView && <NickNameEdit setNicknameView={setNicknameView} propPrefix={setPrefix} propBio={setBio} propNickname={setNickname} />}
       {visible && <Settings setVisible={setVisible} setLogoutView={setLogoutView} setSignoutView={setSignoutView} />}
       <div className={classes.mypagetopbackgroundcolor}>
         <div className={classes.mypagetop}>
@@ -112,7 +124,7 @@ const MyPage = () => {
       </div>
       <div className={classes.profilename}>
         {/* 닉네임 ex)귀티나는 지각생 // props로 가져올 듯 */}
-        <span>{prefix +" "+ nickname}</span>
+        <span>{prefix + " " + nickname}</span>
         {/* 프로필 수정 버튼 -> 컴포넌트 이동 */}
         {ModalOutSide ? <img src="img/pencil.png" alt="" /> :
           <img src="img/pencil.png" alt="" onClick={onNicknameEdit} />
