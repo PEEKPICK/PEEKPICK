f87d8b5868afb9f4ecd8f98d0e8955c7b4f60cdf
Dayoung <everyyoung99@gmail.com>
2023-08-07 12:04:25 +0900

    Style: Peek 지속 시간 수정

--- backend/src/main/java/com/vvs/peekpick/peek/service/PeekServiceImpl.java
+++ backend/src/main/java/com/vvs/peekpick/peek/service/PeekServiceImpl.java
@@ -21,7 +21,7 @@ import java.util.*;
 @RequiredArgsConstructor
 public class PeekServiceImpl implements PeekService {
     private final int MAX_PEEK = 10; // 화면 단에 전닿해주는 Peek 수
-    private final int PEEK_ORIGIN_TIME = 30; // PEEK 기본 지속 시간
+    private final int PEEK_ORIGIN_TIME = 1440; // PEEK 기본 지속 시간
     private final int PEEK_REACTION_TIME = 5; // 좋아요, 싫어요 시 증가되는 시간
     private final Random random = new Random();
 
