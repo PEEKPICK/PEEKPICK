6e0b3844b155e38c33782e7d71a1b666680cd91c
김준형 <wnsgud6232@naver.com>
2023-08-07 22:13:53 +0900

    Chore: Error Boundary 삭제

--- frontend/src/components/pick/Picker.jsx
+++ frontend/src/components/pick/Picker.jsx
@@ -1,15 +1,12 @@
 import classes from "./Picker.module.css";
 import Header from "./Header";
 import FindPicker from "./FindPicker";
-import ErrorBoundary from "../common/ErrorBoundary";
 
 function Picker() {
   return (
     <div className={classes.pickerMain}>
-      <ErrorBoundary>
-        <Header></Header>
-        <FindPicker></FindPicker>
-      </ErrorBoundary>
+      <Header></Header>
+      <FindPicker></FindPicker>
     </div>
   );
 }
