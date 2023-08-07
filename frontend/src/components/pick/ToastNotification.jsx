bc445604ef92dcbef8274420e435e697edbd17f3
김동민 <anwjr41@naver.com>
2023-08-08 01:45:43 +0900

    Fix: chat 수락/거절 3번째

--- frontend/src/components/pick/ToastNotification.jsx
+++ frontend/src/components/pick/ToastNotification.jsx
@@ -3,18 +3,30 @@ import { toast } from "react-toastify";
 import classes from "./ToastNotification.module.css";
 import { customAxios } from "../../api/customAxios";
 
-const CustomToast = ({ senderId, message }) => {
-  const handleAccept = () => {
-    const body = {
-      requestSenderId: 1,
-      requestReceiverId: 2,
-      requestTime: "2023-07-31T14:20:00.1685066",
-      response: "Y",
-    };
-    customAxios.put("/picker/chat-reponse", body).then((res) => {});
+const CustomToast = ({ message, senderId, requestTime }) => {
+  // 수락
+  const handleAccept = async () => {
+    //내 ID
     toast.dismiss();
+    try {
+      const res = await customAxios.get("/member/info");
+      const receiverID = res.data.data.avatarId;
+      const body = {
+        requestSenderId: senderId,
+        requestReceiverId: receiverID,
+        requestTime: requestTime,
+        response: "Y",
+      };
+
+      console.log("body", body);
+      const response = await customAxios.put("/picker/chat-reponse", body);
+      console.log("response", response);
+    } catch (error) {
+      console.error(error);
+    }
   };
 
+  // 거절
   const handleReject = () => {
     const body = {
       requestSenderId: 1,
@@ -26,10 +38,10 @@ const CustomToast = ({ senderId, message }) => {
     toast.clearWaitingQueue(senderId);
   };
 
+  //모두 닫기
   const handleRejectAll = () => {
     toast.dismiss();
   };
-
   return (
     <div className={classes.toastMain}>
       <div>{message}</div>
