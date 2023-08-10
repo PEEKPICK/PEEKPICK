cfe22092b7230ab2aaa746bb26d305359f45b7e8
김준형 <wnsgud6232@naver.com>
2023-08-10 12:52:51 +0900

    Feat: 소셜 로그인 버튼 수정 및 테스트 버튼 삭제

--- frontend/src/components/auth/Login.jsx
+++ frontend/src/components/auth/Login.jsx
@@ -1,38 +1,13 @@
-import { useState } from 'react';
-import { authAxios } from '../../api/customAxios';
-import { useNavigate } from 'react-router-dom';
 import { toast } from 'react-hot-toast';
 
 import classes from './style/Login.module.css';
 
 const Login = () => {
-  // 테스트용 상태 관리
-  const [tester, setTester] = useState(1);
-  const navigate = useNavigate();
 
   const loginHandler = (service) => {
     console.log(service)
     window.location.href = `https://i9b309.p.ssafy.io/api/oauth2/authorization/${service}`;
   };
-
-  // 테스트 버튼
-  const test = (id) => {
-    authAxios.post(`/member/login?id=test${id}`)
-      .then(response => {
-        if (response.data.code === '200') {
-          const token = response.data.data
-          localStorage.setItem("jwtToken", token);
-          window.location.replace("/")
-        }
-      })
-      .catch(error => {
-        console.log(error)
-      })
-  }
-
-  const testSignUp = () => {
-    navigate("/userinfo")
-  }
 
   return (
     <div className={classes.container}>
@@ -42,15 +17,7 @@ const Login = () => {
         className={classes.background}
       />
       <div className={classes.buttons}>
-        {/* 이미지로 변경 예정 */}
-        {/* 테스트 버튼 */}
-        <input type="text" placeholder='숫자를 입력하세요' onChange={e => setTester(e.target.value)} />
-        <button onClick={() => test(tester)}>테스트 버튼</button>
-        <button onClick={() => testSignUp()}>테스트 회원가입</button>
-        {/* 테스트 버튼 */}
-        <a href="https://i9b309.p.ssafy.io/api/oauth2/authorization/kakao">
-          <img src="img/kakao_login.png" alt="kakao" />
-        </a>
+        <img src="img/kakao_login.png" alt="kakao" onClick={() => loginHandler('kakao')} />
         {/* <img src="img/naver_login.png" alt="naver" onClick={() => loginHandler('naver')} /> */}
         <img src="img/naver_login.png" alt="naver" onClick={() => toast.error('개발 중인 서비스입니다.')} />
         <img src="img/google_login.png" alt="google" onClick={() => loginHandler('google')} />
