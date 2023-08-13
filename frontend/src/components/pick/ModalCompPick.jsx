dfbb7250bea205aad3164e336c81f37b2d0c009c
김준형 <wnsgud6232@naver.com>
2023-08-13 21:47:18 +0900

    Style: 픽 횟수 div추가

--- frontend/src/components/pick/ModalCompPick.jsx
+++ frontend/src/components/pick/ModalCompPick.jsx
@@ -58,9 +58,11 @@ const ModalComp = () => {
               <span className={classes.nickname}>
                 {isSelectedEmoji.prefix.content} {isSelectedEmoji.nickname}
               </span>
+              <div className={classes.pickCount}>
                 <span style={{ marginRight: "0.2rem" }}>PICK</span>
                 <span style={{ color: "#7d00ff", fontWeight: "700" }}>{isSelectedEmoji.chatCount}</span>
                 <span style={{ marginLeft: "0.2rem" }}>회</span>
+              </div>
               {isSelectedEmoji.bio && isSelectedEmoji.bio.trim() !== "" ? (
                 <p className={classes.intro}>{isSelectedEmoji.bio}</p>
               ) : (
