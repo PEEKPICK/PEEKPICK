fb4ce8b30c2fc78154c79b712a2e5296e66bb561
김준형 <wnsgud6232@naver.com>
2023-08-13 15:27:54 +0900

    Style: 서비스소개 button -> a tag로 변경 & 소셜 로그인 버튼 그림자 변경

--- frontend/src/components/auth/Login.jsx
+++ frontend/src/components/auth/Login.jsx
@@ -22,11 +22,13 @@ const Login = () => {
         className={classes.background}
       />
       <div className={classes.buttons}>
+        <span
+          className={classes.info}
+        >PEEKPICK이 처음이신가요? <a href="/branding">서비스소개 바로가기</a></span>
         <img src="img/kakao_login.png" alt="kakao" onClick={() => loginHandler('kakao')} />
         {/* <img src="img/naver_login.png" alt="naver" onClick={() => loginHandler('naver')} /> */}
         <img src="img/naver_login.png" alt="naver" onClick={() => toast.error('개발 중인 서비스입니다.')} />
         <img src="img/google_login.png" alt="google" onClick={() => loginHandler('google')} />
-        <img src="img/service_information.png" alt="service" onClick={() => goBranding()} />
       </div>
     </div>
   );
