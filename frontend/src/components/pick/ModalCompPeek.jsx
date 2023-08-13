86f397ac423fb2b65642cdedbf3624d5d6cc53bc
Dayoung1014 <58163364+Dayoung1014@users.noreply.github.com>
2023-08-13 15:32:45 +0900

    Fix: Peek 이미지 기능 제거

--- frontend/src/components/pick/ModalCompPeek.jsx
+++ frontend/src/components/pick/ModalCompPeek.jsx
@@ -65,9 +65,6 @@ const ModalComp = () => {
   }, [timeLeft]);
 
 
-  
-
-  
 
   useEffect(() => {
     if (isSelectedEmoji) {
--- frontend/src/components/pick/ModalWrite.jsx
+++ frontend/src/components/pick/ModalWrite.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from "react";
+import React, { useState } from "react";
 import { customAxios } from "../../api/customAxios";
 import classes from './ModalWrite.module.css';
 import Modal from "react-modal";
@@ -13,17 +13,17 @@ const ModalWrite = ({emojiCall}) => {
   // 글 작성에 필요한 데이터
   // 그냥 창을 닫았을 경우, 이 작업을 해주지 않으면, 다시 글 작성을 할 때, 데이터가 남아 있음.
   const [writeData, setWriteData] = useState("");
-  const [imgData, setImgData] = useState("");
-  const imageInput = useRef();
+  //const [imgData, setImgData] = useState("");
+  //const imageInput = useRef();
 
-  const [imgFile, setImgFile] = useState("");
+  //const [imgFile, setImgFile] = useState("");
   const handleWrite = (e) => {
     setWriteData(e.target.value);
   }
   const handleCloseModal = () => {
-    setImgFile("");
+    //setImgFile("");
     setWriteData("");
-    setImgData("");
+    //setImgData("");
     dispatch(modalsActions.closeModal())
   }
   const postWrite = () => {
@@ -39,23 +39,23 @@ const ModalWrite = ({emojiCall}) => {
     f.append("content", writeData);
     f.append("longitude",userPos.point.x );
     f.append("latitude", userPos.point.y);
-    f.append("img", imgData);
+    //f.append("img", imgData);
 
     setTimeout(() => {
       customAxios.post("/peek/write", f)
       .then((response) => {
         console.log(response);
         toast.success("PEEK 성공");
-        setImgFile("");
+        //setImgFile("");
         setWriteData("");
-        setImgData("");
+        //setImgData("");
         dispatch(modalsActions.closeModal())
         emojiCall();
       })
       .catch((response) => {
-        setImgFile("");
+        //setImgFile("");
         setWriteData("");
-        setImgData("");
+        //setImgData("");
         console.log(response);
         toast.error("ERROR");
         dispatch(modalsActions.closeModal())
@@ -63,24 +63,24 @@ const ModalWrite = ({emojiCall}) => {
     }, 2000);
    
   }
-  const imgAccept = (event) => {
-    const selectedImage = event.target.files[0];
-    const reader = new FileReader();
+  // const imgAccept = (event) => {
+  //   const selectedImage = event.target.files[0];
+  //   const reader = new FileReader();
 
-    if (selectedImage) {
-      console.log('Selected image:', selectedImage);
-      setImgData(selectedImage)
-      reader.readAsDataURL(selectedImage);
-      reader.onloadend = () => {
-        setImgFile(reader.result);
-        console.log(imgFile)
-      };
-    }
-  };
+  //   if (selectedImage) {
+  //     console.log('Selected image:', selectedImage);
+  //     setImgData(selectedImage)
+  //     reader.readAsDataURL(selectedImage);
+  //     reader.onloadend = () => {
+  //       setImgFile(reader.result);
+  //       console.log(imgFile)
+  //     };
+  //   }
+  // };
 
-  const onCickImageUpload = () => {
-    imageInput.current.click();
-  };
+  // const onCickImageUpload = () => {
+  //   imageInput.current.click();
+  // };
   return (
 
     <>
@@ -99,13 +99,13 @@ const ModalWrite = ({emojiCall}) => {
           <div className={classes.content}>
             <textarea onChange={handleWrite} className={classes.text} ></textarea>
           </div>
-          <div className={classes.name}>
+          {/* <div className={classes.name}>
             {imgData.name}
-          </div>
-          <div className={classes.imgthrow}>
+          </div> */}
+          {/* <div className={classes.imgthrow}>
             <button onClick={onCickImageUpload} className={classes.btn}>이미지업로드</button>
             <input ref={imageInput} type="file" accept="image/*" onChange={imgAccept} className={classes.imginput} />
-          </div>
+          </div> */}
           <div className={classes.imgthrow}>
             <button className={classes.button} onClick={postWrite}>입력 완료</button>
           </div>
