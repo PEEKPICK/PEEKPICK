c760d319d862b05af5a8417fd28b87c8391275e0
김동민 <anwjr41@naver.com>
2023-08-16 13:17:55 +0900

    Feat: App.ver 확인

--- frontend/src/components/mypages/Announcement.jsx
+++ frontend/src/components/mypages/Announcement.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import classes from './Announcement.module.css';
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import classes from "./Announcement.module.css";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 const Announcement = () => {
   // 상태(state)를 정의하여 내용의 표시 여부를 저장합니다.
   const [contentVisible, setContentVisible] = useState({});
@@ -10,7 +10,7 @@ const Announcement = () => {
   const toggleContent = (id) => {
     setContentVisible((prevVisible) => ({
       ...prevVisible,
-      [id]: !prevVisible[id]
+      [id]: !prevVisible[id],
     }));
   };
 
@@ -28,64 +28,110 @@ const Announcement = () => {
           <span className={classes.noticedate}>2023년 8월 8일</span>
           <div className={classes.noticetextimg}>
             <p className={classes.noticetext}>중간 발표 최종.ver</p>
-            {contentVisible[1] ?
-              <img src="img/up.png" alt="" onClick={() => { toggleContent(1) }} /> :
-              <img src="img/down.png" alt="" onClick={() => { toggleContent(1) }} />
-            }
+            {contentVisible[1] ? (
+              <img
+                src="img/up.png"
+                alt=""
+                onClick={() => {
+                  toggleContent(1);
+                }}
+              />
+            ) : (
+              <img
+                src="img/down.png"
+                alt=""
+                onClick={() => {
+                  toggleContent(1);
+                }}
+              />
+            )}
           </div>
         </div>
-        {contentVisible[1] && <div className={classes.content}>
-          데이터 전달 로직 수정중..
-        </div>}
+        {contentVisible[1] && <div className={classes.content}>데이터 전달 로직 수정중..</div>}
       </div>
       <div className={classes.noticecontent}>
         <div className={classes.noticeitem}>
           <span className={classes.noticedate}>2023년 8월 9일</span>
           <div className={classes.noticetextimg}>
             <p className={classes.noticetext}>중간 발표 최종의 최종.ver</p>
-            {contentVisible[2] ?
-              <img src="img/up.png" alt="" onClick={() => { toggleContent(2) }} /> :
-              <img src="img/down.png" alt="" onClick={() => { toggleContent(2) }} />
-            }
+            {contentVisible[2] ? (
+              <img
+                src="img/up.png"
+                alt=""
+                onClick={() => {
+                  toggleContent(2);
+                }}
+              />
+            ) : (
+              <img
+                src="img/down.png"
+                alt=""
+                onClick={() => {
+                  toggleContent(2);
+                }}
+              />
+            )}
           </div>
         </div>
-        {contentVisible[2] && <div className={classes.content}>
-          이게 왜 안돼..?
-        </div>}
+        {contentVisible[2] && <div className={classes.content}>이게 왜 안돼..?</div>}
       </div>
       <div className={classes.noticecontent}>
         <div className={classes.noticeitem}>
           <span className={classes.noticedate}>2023년 8월 10일</span>
           <div className={classes.noticetextimg}>
             <p className={classes.noticetext}>중간 발표 최종의 최종의 최종.ver</p>
-            {contentVisible[3] ?
-              <img src="img/up.png" alt="" onClick={() => { toggleContent(3) }} /> :
-              <img src="img/down.png" alt="" onClick={() => { toggleContent(3) }} />
-            }
+            {contentVisible[3] ? (
+              <img
+                src="img/up.png"
+                alt=""
+                onClick={() => {
+                  toggleContent(3);
+                }}
+              />
+            ) : (
+              <img
+                src="img/down.png"
+                alt=""
+                onClick={() => {
+                  toggleContent(3);
+                }}
+              />
+            )}
           </div>
         </div>
-        {contentVisible[3] && <div className={classes.content}>
-          아아..시간이 되버렸습니다..
-          저는 최선을 다했어요..
-          잘 되겠죠..?
-        </div>}
+        {contentVisible[3] && (
+          <div className={classes.content}>
+            아아..시간이 되버렸습니다.. 저는 최선을 다했어요.. 잘 되겠죠..?
+          </div>
+        )}
       </div>
       <div className={classes.noticecontent}>
         <div className={classes.noticeitem}>
-          <span className={classes.noticedate}>2023년 8월 10일</span>
+          <span className={classes.noticedate}>2023년 8월 16일</span>
           <div className={classes.noticetextimg}>
-            <p className={classes.noticetext}>중간 발표 진짜 진짜 최종의 최종의 최종.ver</p>
-            {contentVisible[4] ?
-              <img src="img/up.png" alt="" onClick={() => { toggleContent(4) }} /> :
-              <img src="img/down.png" alt="" onClick={() => { toggleContent(4) }} />
-            }
+            <p className={classes.noticetext}>App.Ver</p>
+            {contentVisible[4] ? (
+              <img
+                src="img/up.png"
+                alt=""
+                onClick={() => {
+                  toggleContent(4);
+                }}
+              />
+            ) : (
+              <img
+                src="img/down.png"
+                alt=""
+                onClick={() => {
+                  toggleContent(4);
+                }}
+              />
+            )}
           </div>
         </div>
-        {contentVisible[4] && <div className={classes.content}>
-          제발 제발 제발 제발 에러만 뜨지 말아주세요 제발!!! 제발요 ㅠㅠㅠㅠㅠㅠㅠㅠ
-        </div>}
+        {contentVisible[4] && <div className={classes.content}>23.08.16.13.15 - locChmod</div>}
       </div>
     </div>
   );
-}
+};
 export default Announcement;
