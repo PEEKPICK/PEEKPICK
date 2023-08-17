d04618e6bd415272487b7ce063b1b04eb5e98651
Dayoung <everyyoung99@gmail.com>
2023-08-17 10:39:12 +0900

    Fix: special 기준 수정

--- backend/src/main/java/com/vvs/peekpick/peek/service/PeekServiceImpl.java
+++ backend/src/main/java/com/vvs/peekpick/peek/service/PeekServiceImpl.java
@@ -287,7 +287,7 @@ public class PeekServiceImpl implements PeekService {
 //            if (Duration.between(peekRedisDto.getWriteTime(), updatedFinishTime).toMinutes() >= 80) {
 //                special = true;
 //            }
-            if(likeCnt+disLikeCnt >= 10) special = true;
+            if(likeCnt+disLikeCnt >= 5) special = true;
             if (Duration.between(peekRedisDto.getWriteTime(), updatedFinishTime).toHours() >= PEEK_MAX_HOUR) {
                 updatedFinishTime = peekRedisDto.getWriteTime().plusHours(PEEK_MAX_HOUR);
             }
